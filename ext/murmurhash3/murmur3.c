234a326d1da0b849062f0733d63deb0174cf24aa
leaf corcoran <leafot@gmail.com>
2013-09-10 13:53:36 -0700

    add stdint.h

--- ext/murmurhash3/murmur3.c
+++ ext/murmurhash3/murmur3.c
@@ -1,4 +1,5 @@
 #include <ruby.h>
+#include <stdint.h>
 /*-----------------------------------------------------------------------------
  * MurmurHash3 was written by Austin Appleby, and is placed in the public
  * domain. The author hereby disclaims copyright to this source code.
